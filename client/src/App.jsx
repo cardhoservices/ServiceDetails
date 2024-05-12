d2d2fbba5f359a54bd22737775dce1e570c381ee
cardhoservices <techcardho@gmail.com>
2024-05-12 17:43:03 +0530

    show name on form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -59,7 +59,6 @@ function SubscriptionTracker() {
             console.error(error);
         }
     }
-    // console.log(data)
     return (
         <div className="container">
             <h1>CarDho Subscription Tracker</h1>
@@ -99,7 +98,7 @@ function SubscriptionTracker() {
                         <h2>Subscription Details</h2>
                         <p>{`Start Date: ${StartDate}`}</p>
                         <p>Exterior Cleaning: Daily</p>
-                        <p>{`Interior Cleaning Dates: ${finaldata.interriorfirst.split("T")[0]+" , "+finaldata.interriorsecond.split("T")[0] +" , "+ finaldata.interriorthird.split("T")[0] +" , "+ finaldata.interriorfourth.split("T")[0]}`}</p>
+                        <p>Interior Cleaning Dates: {finaldata.interriorfirst.split("T")[0]}, {finaldata.interriorsecond?finalfinaldata.interriorsecond.split("T")[0]:"NA"}, {finaldata.interriorthird?finalfinaldata.interriorthird.split("T")[0]:"NA"}, {finaldata.interriorfourth?finalfinaldata.interriorfourth.split("T")[0]:"NA"}</p>
                         <p>{`Pressure Wash: ${calculateNextPressureWash(StartDate)}`}</p>
                         <p>{`Valid Till: ${calculateSubscriptionValidTill(StartDate)}`}</p>
                     </>
